3ef4762c08988a2589e547b92bac9c17c3ad1424
yuji-okayama <y-okayama@tribeck.jp>
2020-07-31 12:21:48 +0900

    routerのテスト

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,30 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Switch, Route, Link } from 'react-router-dom';
+import Top from './pages/Top';
+import About from './pages/About';
+import User from './pages/User';
 
 function App() {
+  const randumNum = (min: number, max: number): number => {
+    return Math.floor(Math.random() * 10);
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+        <header>
+          <nav>
+            <Link to="/">index</Link>
+            <Link to="/about">about</Link>
+            <Link to={`/user/${randumNum(1, 10)}`}>user</Link>
+          </nav>
+        </header>
+        <Switch>
+          <Route path="/" exact children={<Top />} />
+          <Route path="/about" children={<About />} />
+          <Route path="/user/:id" children={<User />} />
+        </Switch>
+      </BrowserRouter>
     </div>
   );
 }
