562bb501d9e6fd42e75bb6cfb5be502c0897bbd3
yuji okayama <yujiokayama@yujinoMacBook-Pro.local>
2020-08-09 22:16:36 +0900

    ジェネリクスについて

--- src/pages/typescript-basic.tsx
+++ src/pages/typescript-basic.tsx
@@ -1,72 +1,46 @@
-import * as React from 'react';
+import * as React from "react";
 
-import classNames from 'classnames';
+import classNames from "classnames";
 
 function TypeScriptBasic() {
   /**
-   * 関数型コンポーネントのためのフック
+   * typeとinterface
+   * typeを使おうね
    */
 
   /**
-   * 配列やオブジェクトで動的なクラスを指定
+   * ジェネリクス
    */
-  const buttonClass = classNames({
-    btn: true,
-    'btn-primary': true
-  });
+  type Foo<S, T> = {
+    foo: S;
+    bar: T;
+  }
 
-  const buttonClass2 = classNames(['hoge', 'fuga']);
-
-  /**
-   * useStateを使用したステート管理
-   */
-  const [count, setCount] = React.useState(0);
-  const handleClick = () => setCount(count + 1);
-
-  /**
-   * フォームの双方向バインディング
-   */
-  const [name, setName] = React.useState('');
-  const handleInput = (e: any) => setName(e.target.value);
-
-  /**
-   * コンポーネントメソッド
-   */
-  const sayHello = () => {
-    console.log(`Hello, ${name}!`);
+  // 変数・定数
+  const obj: Foo<number, string> = {
+    foo: 3,
+    bar: "hi",
   };
 
-  /**
-   * ref
-   */
-
-  /**
-   * created
-   */
-  sayHello();
+  // クラス
+  class Fuga<T> {
+    constructor(obj: T) {
+    }
+  }
+  const obj1 = new Fuga<string>('foo');
+  
+  // 関数
+  function func<T>(arg: T): T {
+    return arg
+  }
+  
+  func<number>(3);
 
   return (
-    // React.Fragmentでルート要素を返却する
     <React.Fragment>
       <h1 className="text-4xl mb-5">TypeScriptBasic</h1>
 
-      <h2 className="text-3xl">配列やオブジェクトで動的なクラスを指定</h2>
-      <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
-        Button
-      </button>
-      <button className={buttonClass2}>Button</button>
-
-      <h2 className="text-3xl">ステートを管理する</h2>
-      <button onClick={handleClick}>カウント:{count}</button>
-
-      <h2 className="text-3xl">フォームの双方向バインディング</h2>
-      <p>双方向バインディング:{name}</p>
-      <input
-        className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-        type="text"
-        onChange={handleInput}
-        value={name}
-      />
+      <h2 className="text-3xl">ジェネリクス</h2>
     </React.Fragment>
   );
 }
